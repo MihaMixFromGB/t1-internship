c1204effd00e6acfecc1f7d21ed6bada4581f27e
Anton Dehterev <dechterev@mail.ru>
2024-03-20 00:09:13 +0700

    fix common styles

--- src/features/tag/set/set-tag.ui.tsx
+++ src/features/tag/set/set-tag.ui.tsx
@@ -3,7 +3,7 @@ import css from './set-tag.module.css';
 import type { SetTagProps } from './set-tag.types';
 
 export const SetTag: React.FC<SetTagProps> = ({ tag, active, toggle }) => {
-  const classes = clsx(css.tag, {
+  const classes = clsx(css.tag, 'input-hidden', {
     [css.active]: active,
   });
 
