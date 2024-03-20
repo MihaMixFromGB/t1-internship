fc9c8c344646301e539a6174e00de77575051f47
Anton Dehterev <dechterev@mail.ru>
2024-03-20 23:35:13 +0700

    fix the slider

--- src/widgets/slider/slider.ui.tsx
+++ src/widgets/slider/slider.ui.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { useState } from 'react';
 import css from './slider.module.css';
 import { SliderProps } from './slider.types';
@@ -10,20 +11,32 @@ export const Slider: React.FC<SliderProps> = ({ images }) => {
       <img src={images[activeIdx]} width={520} />
       <div className={css.slider__gallery}>
         {images.map((src, idx) => (
-          <label key={idx} className='input-hidden'>
-            <img
-              className={`${css.slider__gallery__img} ${activeIdx === idx ? css.active : ''}`}
-              src={src}
-              width={70}
-            />
-            <input
-              type='radio'
-              name='slider'
-              onChange={() => setActiveIdx(idx)}
-            ></input>
-          </label>
+          <GalleryItem
+            key={idx}
+            src={src}
+            active={idx === activeIdx}
+            toggle={() => setActiveIdx(idx)}
+          />
         ))}
       </div>
     </div>
+  );
+};
+
+type GalleryItemProps = {
+  src: string;
+  active: boolean;
+  toggle: () => void;
+};
+const GalleryItem: React.FC<GalleryItemProps> = ({ src, active, toggle }) => {
+  const classes = clsx(css.slider__gallery__img, {
+    [css.active]: active,
+  });
+
+  return (
+    <label className='input-hidden'>
+      <img className={classes} src={src} width={70} />
+      <input type='radio' name='slider' onChange={toggle}></input>
+    </label>
   );
 };
