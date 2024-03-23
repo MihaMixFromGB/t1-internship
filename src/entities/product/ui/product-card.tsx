228f186d39c012088c5be7c59ed1106ed33c9d7b
Anton Dehterev <dechterev@mail.ru>
2024-03-24 00:14:33 +0700

    add a link to a product card

--- src/entities/product/ui/product-card.tsx
+++ src/entities/product/ui/product-card.tsx
@@ -1,22 +1,26 @@
+import { Link } from 'react-router-dom';
+import { paths } from '@/shared/model';
 import { Paragraph } from '@/shared/ui';
 import { ProductCardProps } from '../product.types';
 import css from './product-card.module.css';
 
 export const ProductCard: React.FC<ProductCardProps> = ({ info }) => {
   return (
-    <article aria-label={`${info.title}. Price ${info.price} USD`}>
-      <img
-        className={css.product__img}
-        src={info.thumbnail}
-        alt=''
-        aria-hidden={true}
-      />
-      <Paragraph className={css.product__label} aria-hidden={true}>
-        {info.title}
-      </Paragraph>
-      <Paragraph lead aria-hidden={true}>
-        {info.price}$
-      </Paragraph>
-    </article>
+    <Link to={paths.product(info.id)}>
+      <article aria-label={`${info.title}. Price ${info.price} USD`}>
+        <img
+          className={css.product__img}
+          src={info.thumbnail}
+          alt=''
+          aria-hidden={true}
+        />
+        <Paragraph className={css.product__label} aria-hidden={true}>
+          {info.title}
+        </Paragraph>
+        <Paragraph lead aria-hidden={true}>
+          {info.price}$
+        </Paragraph>
+      </article>
+    </Link>
   );
 };
